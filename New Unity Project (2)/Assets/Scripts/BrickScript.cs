bee7f8c5398ba224b2de9813d4851e0901d72a02
Naya Al Tahan <naya.altahan00@gmail.com>
2020-09-18 22:44:42 +0200

    add some

---
+++ New Unity Project (2)/Assets/Scripts/BrickScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+    private GameObject _gameObject;
+
+    private Material _material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameObject = GetComponent<GameObject>();
+        _material = GetComponent<Material>();
+    }
+
+    public void OnCollisionEnter(Collision hit)
+    {
+        gameObject.SetActive(false);
+    }
+}
